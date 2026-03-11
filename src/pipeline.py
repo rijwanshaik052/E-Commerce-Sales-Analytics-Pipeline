1622fde7dc3f135e267d4d7fe4876890eaf853fd
rijwanshaik <rijwansk052@gmail.com>
2026-03-11 11:48:15 +0530

    created postgres db and pipeline container using docker and docker compose

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,18 +1,89 @@
-from db.run_migrations import run_all_migrations
+# from db.run_migrations import run_all_migrations
+# import psycopg2
+
+# def main():
+
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         dbname="olist_db",
+#         user="rijwan",
+#         password="rijwan123"
+#     )
+
+#     run_all_migrations(conn)
+
+#     print("Tables ready. Starting pipeline...")
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+import time
 import psycopg2
+from db.run_migrations import run_all_migrations
+
+def connect_db():
+    while True:
+        try:
+            conn = psycopg2.connect(
+                host="postgres",
+                port=5432,
+                dbname="olist_db",
+                user="rijwan",
+                password="rijwan123"
+            )
+            print("Connected to Postgres")
+            return conn
+        except psycopg2.OperationalError:
+            print("Postgres not ready... retrying")
+            time.sleep(5)
+
+            run_all_migrations(conn)
+
+            print("Tables ready. Starting pipeline...")
+
+
 
 def main():
-
-    conn = psycopg2.connect(
-        host="localhost",
-        dbname="olist_db",
-        user="rijwan",
-        password="rijwan123"
-    )
-
-    run_all_migrations(conn)
-
-    print("Tables ready. Starting pipeline...")
+    conn = connect_db()
+    # continue pipeline
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import os
+# import psycopg2
+
+# def main():
+
+#     conn = psycopg2.connect(
+#         host=os.getenv("DB_HOST"),
+#         port=os.getenv("DB_PORT"),
+#         database=os.getenv("DB_NAME"),
+#         user=os.getenv("DB_USER"),
+#         password=os.getenv("DB_PASSWORD")
+#     )
+
+#     print("Connected to Postgres")
+
+#     conn.close()
+
+# if __name__ == "__main__":
+#     main()
